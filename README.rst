2ef83667dc87cabfdc4bf086568a26fbab39ae64
Praveen Garg <praveen.garg@nerdapplabs.com>
2018-06-21 16:41:02 +0530

    DHT11 plugin for Rasperry Pi

---
+++ README.rst
@@ -0,0 +1,5 @@
+======================
+foglamp-south-dht11 Pi
+======================
+
+FogLAMP South Plugin for DHT11 (For Raspberry Pi)
---
+++ test/test_dht11.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# FOGLAMP_BEGIN
+# See: http://foglamp.readthedocs.io/
+# FOGLAMP_END
+
+import pytest
+
+from python.foglamp.plugins.south.dht11 import dht11
+
+__author__ = "Praveen Garg"
+__copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
+__license__ = "Apache 2.0"
+__version__ = "${VERSION}"
+
+config = dht11._DEFAULT_CONFIG
+
+
+def test_plugin_contract():
+    # Evaluates if the plugin has all the required methods
+    assert callable(getattr(dht11, 'plugin_info'))
+    assert callable(getattr(dht11, 'plugin_init'))
+    assert callable(getattr(dht11, 'plugin_start'))
+    assert callable(getattr(dht11, 'plugin_shutdown'))
+    assert callable(getattr(dht11, 'plugin_reconfigure'))
+
+
+def test_plugin_info():
+    assert dht11.plugin_info() == {
+        'name': 'DHT11 GPIO',
+        'version': '1.0',
+        'mode': 'poll',
+        'type': 'south',
+        'interface': '1.0',
+        'config': config
+    }
+
+
+def test_plugin_init():
+    assert dht11.plugin_init(config) == config
+
+
+@pytest.mark.skip(reason="To be implemented")
+def test_plugin_poll():
+    pass
+
+
+@pytest.mark.skip(reason="To be implemented")
+def test_plugin_reconfigure():
+    pass
+
+
+@pytest.mark.skip(reason="To be implemented")
+def test_plugin_shutdown():
+    pass
